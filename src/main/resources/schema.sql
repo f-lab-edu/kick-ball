f99d78a08b4cc869cccec8ba8cda727224ccad98
Shin_0602_ <80821744+ShinjaeKwon@users.noreply.github.com>
2023-09-22 17:21:07 +0900

    docs : change ddl type

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE stadium (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) not null,
     address VARCHAR(100) not null,
     size VARCHAR(50) not null,
