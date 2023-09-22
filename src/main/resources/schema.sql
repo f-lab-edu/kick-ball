1d663ccd937b24f5a2f9009ff26e704a347a6b0c
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-09-22 19:50:18 +0900

    docs : stadium ddl cherry-pick in member ddl

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,17 @@
+CREATE TABLE stadium (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) not null,
+    address VARCHAR(100) not null,
+    size VARCHAR(50) not null,
+    guidelines CLOB,
+    parking BOOLEAN not null,
+    airConditioning BOOLEAN not null,
+    showers BOOLEAN not null,
+    ballRental BOOLEAN not null,
+    vestRental BOOLEAN not null,
+    shoesRental BOOLEAN not null
+);
+
 CREATE TABLE members (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) not null,
