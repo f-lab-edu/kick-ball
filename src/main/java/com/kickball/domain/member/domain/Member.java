5d1face784a441d95a804e4471937d8e174f853b
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-09-22 17:23:55 +0900

    feat : add Member entity

---
+++ src/main/java/com/kickball/domain/member/domain/Member.java
@@ -0,0 +1,32 @@
+package com.kickball.domain.member.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "members")
+@Entity
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "username", nullable = false, length = 50)
+	private String username;
+
+	@Column(name = "password", nullable = false, length = 100)
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false, length = 50)
+	private RoleType role;
+
+	@Column(name = "provider", nullable = false, length = 50)
+	private String provider;
+}
