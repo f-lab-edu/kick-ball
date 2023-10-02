328e7ce6cbdb180762bbbc9ae545c59974636c3f
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-10-02 17:02:00 +0900

    feat : add stadium save dto

---
+++ src/main/java/com/kickball/domain/stadium/dto/response/StadiumResponse.java
@@ -0,0 +1,22 @@
+package com.kickball.domain.stadium.dto.response;
+
+import com.kickball.domain.stadium.domain.Stadium;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class StadiumResponse {
+
+	private Long id;
+
+	public static StadiumResponse toDto(Long id) {
+		return new StadiumResponse(id);
+	}
+
+}
