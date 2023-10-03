70c339a522c40fd09def6029972424bceeb49c41
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-10-04 04:53:00 +0900

    refactor : remove hibernate dependency
    * hibernate 의존성 제거

--- src/main/java/com/kickball/stadium/dto/request/StadiumSaveRequest.java
+++ src/main/java/com/kickball/stadium/dto/request/StadiumSaveRequest.java
@@ -1,39 +1,43 @@
 package com.kickball.stadium.dto.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.Range;
+import javax.validation.constraints.Size;
 
 import com.kickball.stadium.domain.Stadium;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class StadiumSaveRequest {
 
 	@NotBlank(message = "구장 이름은 공백이면 안됩니다.")
-	@Length(min = 1, max = 100, message = "구장 이름의 길이는 1이상, 100이하여야 합니다.")
+	@Size(min = 1, max = 100, message = "구장 이름의 길이는 1이상, 100이하여야 합니다.")
 	private String name;
 
 	@NotBlank(message = "주소는 공백이면 안됩니다.")
-	@Length(min = 1, max = 100, message = "주소의 길이는 1이상, 100이하여야 합니다.")
+	@Size(min = 1, max = 100, message = "주소의 길이는 1이상, 100이하여야 합니다.")
 	private String address;
 
-	@Range(min = 1, max = 150, message = "가로 길이는 1이상, 150이하여야 합니다.")
+	@Min(value = 1, message = "가로 길이는 1 이상 이여야 합니다.")
+	@Max(value = 150, message = "가로 길이는 150 이하 이여야 합니다.")
 	private int width;
 
-	@Range(min = 1, max = 150, message = "세로 길이는 1이상, 150이하여야 합니다.")
+	@Min(value = 1, message = "세로 길이는 1 이상 이여야 합니다.")
+	@Max(value = 150, message = "세로 길이는 150 이하 이여야 합니다.")
 	private int length;
 
-	@Range(min = 10_000, max = 300_000, message = "2시간 가격은 10,000원 이상, 300,000원 이하여야 합니다.")
+	@Min(value = 300_000, message = "2시간 가격은 300,000원 이하여야 합니다.")
+	@Max(value = 10_000, message = "2시간 가격은 10,000원 이상이여야 합니다.")
 	private int twoHourRate;
 
 	@NotBlank(message = "안내문은 공백이면 안됩니다.")
