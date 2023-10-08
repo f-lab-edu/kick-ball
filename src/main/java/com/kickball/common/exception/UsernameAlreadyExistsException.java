3fa4df953798ffb18e4832985bad1feadb5e94b0
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-10-08 17:57:17 +0900

    feat : add unique field custom exception

---
+++ src/main/java/com/kickball/common/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.kickball.common.exception;
+
+
+public class UsernameAlreadyExistsException extends RuntimeException{
+	public UsernameAlreadyExistsException(String message) {
+		super(message);
+	}
+}
